cf23774a8ea0b5d688a8d3c4629b665357715ec7
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2021-07-12 09:57:06 +0200

    try to fix GET issue

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -50,6 +50,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ["vue-agile"]
+    transpile: ["vue-agile"],
+    publicPath: "/exams/"
   }
 };
